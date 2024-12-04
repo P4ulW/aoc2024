5f3dac171ef6327c1909771d99427702d77b2b1b
Paul Werner <paul.werner@stud.uni-goettingen.de>
2024-12-04 11:12:45 +0100

    finished day 4

--- day4/day4.py
+++ day4/day4.py
@@ -2,14 +2,75 @@ from rich import print
 from collections import deque
 
 
+masses: list[list[list[str]]] = [
+    [
+        ['M', '.', 'M'],
+        ['.', 'A', '.'],
+        ['S', '.', 'S'],
+    ],
+    [
+        ['M', '.', 'S'],
+        ['.', 'A', '.'],
+        ['M', '.', 'S'],
+    ],
+    [
+        ['S', '.', 'M'],
+        ['.', 'A', '.'],
+        ['S', '.', 'M'],
+    ],
+    [
+        ['S', '.', 'S'],
+        ['.', 'A', '.'],
+        ['M', '.', 'M'],
+    ],
+]
+
+
 def main():
+    print('Start')
     char_matrix = get_char_matrix_file('./inputs.txt')
     # part1
     res = find_xmas_in_matrix(char_matrix)
     print(f'part1: {res}')
 
     # part2
+    res = find_mas_in_matrix(char_matrix)
+    print(f'part2: {res}')
     pass
+
+
+def current_is_mas(matrix, x, y):
+    size_y = len(matrix)
+    size_x = len(matrix[0])
+    match = 0
+    if not matrix[y][x] in ['M', 'S']:
+        return match
+    if x+3 <= size_x and y+3 <= size_y:
+        to_test = [[matrix[y+j][x+i] if not (((i == 0 or i == 2) and j == 1) or (i == 1 and (j == 0 or j == 2))) else '.'
+                    for i in range(3)]
+                   for j in range(3)]
+        if to_test in masses:
+            match = 1
+        # for line in to_test:
+        #     print(line)
+        # if match:
+        #     print('matches')
+        # print('\n')
+        pass
+
+    return match
+
+
+def find_mas_in_matrix(char_matrix: list[list[str]]):
+    size_y = len(char_matrix)
+    size_x = len(char_matrix[0])
+    mask = [[0 for i in range(size_x)] for j in range(size_y)]
+    # matches = 0
+    matches = [[current_is_mas(char_matrix, x, y)
+                for x in range(size_x)]
+               for y in range(size_y)]
+    matches = sum([num for line in matches for num in line])
+    return matches
 
 
 def get_char_matrix_file(filename: str) -> list[list[str]]:
@@ -24,13 +85,11 @@ def find_xmas_in_matrix(char_matrix: list[list[str]]):
     size_y = len(char_matrix)
     size_x = len(char_matrix[0])
     mask = [[0 for i in range(size_x)] for j in range(size_y)]
-    matches = 0
-    for y in range(size_y):
-        for x in range(size_x):
-            matched = search_word_in_matrix(
-                char_matrix, 'XMAS', x, y)
-            mask[y][x] = matched
-            matches += matched
+    # matches = 0
+    matches = [[search_word_in_matrix(char_matrix, 'XMAS', x, y)
+                for x in range(size_x)]
+               for y in range(size_y)]
+    matches = sum([num for line in matches for num in line])
     return matches
 
 
@@ -40,6 +99,9 @@ def search_word_in_matrix(matrix, word, x, y):
     length = len(word)
     word_reverse = word[::-1]
     matches = 0
+
+    if matrix[y][x] not in ["X", 'S']:
+        return 0
 
     # forward
     if x+length <= size_x:
@@ -69,4 +131,5 @@ def search_word_in_matrix(matrix, word, x, y):
 
 
 if __name__ == "__main__":
-    main()
+    from timeit import timeit
+    print(timeit(main, number=100))
