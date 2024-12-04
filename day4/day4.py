c8d1770fbc66e5cd94f2772a6264a4dccdc8c2a4
Paul Werner <paul.werner@stud.uni-goettingen.de>
2024-12-04 10:32:17 +0100

    d4p1 done

--- day4/day4.py
+++ day4/day4.py
@@ -3,11 +3,12 @@ from collections import deque
 
 
 def main():
-    char_matrix = get_char_matrix_file('./test_inputs.txt')
-    for line in char_matrix:
-        print(line)
+    char_matrix = get_char_matrix_file('./inputs.txt')
+    # part1
     res = find_xmas_in_matrix(char_matrix)
-    print(res)
+    print(f'part1: {res}')
+
+    # part2
     pass
 
 
@@ -30,8 +31,6 @@ def find_xmas_in_matrix(char_matrix: list[list[str]]):
                 char_matrix, 'XMAS', x, y)
             mask[y][x] = matched
             matches += matched
-        print('\n')
-    print(mask)
     return matches
 
 
@@ -39,58 +38,30 @@ def search_word_in_matrix(matrix, word, x, y):
     size_y = len(matrix)
     size_x = len(matrix[0])
     length = len(word)
-    # word = [char for char in word]
     word_reverse = word[::-1]
     matches = 0
 
     # forward
     if x+length <= size_x:
-        to_test = ''.join([matrix[y][i] for i in range(x, x+length)])
-        print(f'fore: \t{to_test}')
+        to_test = ''.join([matrix[y][x+i] for i in range(length)])
         if to_test == word or to_test == word_reverse:
             matches += 1
-
-    # # backwards
-    # # minus one because range(0, length) = [0 ... length - 1]
-    # print(x-length)
-    # if x-length >= -1:
-    #     to_test = ''.join([matrix[y][x-i] for i in range(length)])
-    #     print(f'back: \t{to_test}')
-    #     if to_test == word or to_test == word_reverse:
-    #         matches += 1
 
     # down
     if y+length <= size_y:
-        to_test = ''.join([matrix[i][x] for i in range(y, y+length)])
-        print(f'down: \t{to_test}')
+        to_test = ''.join([matrix[y+i][x] for i in range(length)])
         if to_test == word or to_test == word_reverse:
             matches += 1
 
-    # # up
-    # if y-length >= -1:
-    #     to_test = ''.join([matrix[y-i][x] for i in range(length)])
-    #     print(f'up: \t{to_test}')
-    #     if to_test == word or to_test == word_reverse:
-    #         matches += 1
-
-    # diagonal for
+    # diag
     if y+length <= size_y and x+length <= size_x:
         to_test = ''.join([matrix[y+i][x+i] for i in range(length)])
-        print(f'diag: \t\t{to_test}')
         if to_test == word or to_test == word_reverse:
             matches += 1
 
-    # # diagonal back
-    # if y-length >= -1 and x-length >= -1:
-    #     to_test = ''.join([matrix[y-i][x-i] for i in range(length)])
-    #     print(f'diag: \t\t{to_test}')
-    #     if to_test == word or to_test == word_reverse:
-    #         matches += 1
-
     # other diag
-    if y+length < length and x-length >= -1:
+    if y+length <= size_y and x-length >= -1:
         to_test = ''.join([matrix[y+i][x-i] for i in range(length)])
-        print(f'diag2: \t\t{to_test}')
         if to_test == word or to_test == word_reverse:
             matches += 1
 
