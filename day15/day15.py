fcf8137992c0e1029e8c17fd66cb632861022acb
Paul Werner <paul.werner@stud.uni-goettingen.de>
2024-12-16 14:27:59 +0100

    solved day15

--- day15/day15.py
+++ day15/day15.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 DIRECTIONS = {
     '<': (0, -1),
     '>': (0, 1),
@@ -10,6 +11,7 @@ ROBOT = '@'
 EMPTY = '.'
 BOX_LEFT = '['
 BOX_RIGHT = ']'
+
 
 
 def get_warehouse_and_movements(
@@ -172,13 +174,15 @@ def do_movement_wide(
     return new_warehouse, robot_position
 
 
+    
+
 def push_box_wide(
         warehouse: list[list[str]],
         position: tuple[int, int],
-        movement: str
+        movement: str,
 ):
     current_box = warehouse[position[0]][position[1]]
-    new_warehouse = warehouse
+    new_warehouse = [row.copy() for row in warehouse]
     direction = DIRECTIONS[movement]
     new_y = position[0]+direction[0]
     new_x = position[1]+direction[1]
@@ -195,46 +199,77 @@ def push_box_wide(
             new_warehouse[new_y][new_x] = current_box
 
     elif movement in ['^', 'v']:
-        if current_box == BOX_LEFT:
-            if (new_warehouse[new_y][new_x] == WALL
-                    or new_warehouse[new_y][new_x+1] == WALL):
-                return new_warehouse
+        boxes = find_all_boxes(movement, position, warehouse)
+        if movement == '^':
+            boxes = sorted(boxes, reverse=False)
+        else:
+            boxes = sorted(boxes, reverse=True)
+        print(boxes)
 
-            if new_warehouse[new_y][new_x] in (BOX_LEFT, BOX_RIGHT):
-                new_warehouse = push_box_wide(
-                    warehouse, (new_y, new_x), movement)
-
-            if new_warehouse[new_y][new_x+1] in (BOX_LEFT, BOX_RIGHT):
-                new_warehouse = push_box_wide(
-                    warehouse, (new_y, new_x+1), movement)
-
-            if (new_warehouse[new_y][new_x] == EMPTY
-                    and new_warehouse[new_y][new_x+1] == EMPTY):
-                new_warehouse[new_y][new_x] = BOX_LEFT
-                new_warehouse[new_y][new_x+1] = BOX_RIGHT
-                new_warehouse[position[0]][position[1]] = EMPTY
-                new_warehouse[position[0]][position[1]+1] = EMPTY
-
-        elif current_box == BOX_RIGHT:
-            if (new_warehouse[new_y][new_x] == WALL
-                    or new_warehouse[new_y][new_x-1] == WALL):
-                return new_warehouse
-
-            if new_warehouse[new_y][new_x] in (BOX_LEFT, BOX_RIGHT):
-                new_warehouse = push_box_wide(
-                    warehouse, (new_y, new_x), movement)
-
-            if new_warehouse[new_y][new_x-1] in (BOX_LEFT, BOX_RIGHT):
-                new_warehouse = push_box_wide(
-                    warehouse, (new_y, new_x-1), movement)
-
-            if (new_warehouse[new_y][new_x] == EMPTY
-                    and new_warehouse[new_y][new_x-1] == EMPTY):
-                new_warehouse[new_y][new_x] = BOX_RIGHT
-                new_warehouse[new_y][new_x-1] = BOX_LEFT
-                new_warehouse[position[0]][position[1]] = EMPTY
-                new_warehouse[position[0]][position[1]-1] = EMPTY
+        for box in boxes:
+            movePossible = check_box_move(box, movement, new_warehouse)
+            if not movePossible:
+                return warehouse
+            else:
+                y, x = box
+                new_y, new_x = y+direction[0], x+direction[1]
+                new_warehouse[new_y][new_x] = new_warehouse[y][x]
+                new_warehouse[y][x] = EMPTY
+         
     return new_warehouse
+
+
+def find_all_boxes(
+        movement: str,
+        position: tuple[int, int],
+        warehouse: list[list[str]],
+        seen: list[int] = []
+) -> list[tuple[int, int]]:
+    current_box = warehouse[position[0]][position[1]]
+    y, x = position
+    direction = DIRECTIONS[movement]
+    new_y = y+direction[0]
+    new_x = x+direction[1]
+    boxes = []
+
+    boxes.append((y, x))
+
+    if warehouse[new_y][new_x] == WALL:
+        return boxes
+    elif warehouse[new_y][new_x] in (BOX_RIGHT, BOX_LEFT):
+        to_add_boxes = find_all_boxes(movement, (new_y, new_x), warehouse, boxes)
+        for box in to_add_boxes:
+            if box not in boxes:
+                boxes.append(box)
+
+    if current_box == BOX_LEFT and (y, x+1) not in seen:
+        to_add_boxes = find_all_boxes(movement, (y, x+1), warehouse, boxes)
+        for box in to_add_boxes:
+            if box not in boxes:
+                boxes.append(box)
+
+    elif current_box == BOX_RIGHT and (y, x-1) not in seen:
+        to_add_boxes = find_all_boxes(movement, (y, x-1), warehouse, boxes)
+        for box in to_add_boxes:
+            if box not in boxes:
+                boxes.append(box)
+
+
+    return boxes
+
+
+def check_box_move(
+        box: tuple[int, int],
+        movement: str,
+        warehouse: list[list[str]]
+) -> bool:
+    y, x = box
+    direction = DIRECTIONS[movement]
+    new_y, new_x = y+direction[0], x+direction[1]
+    if warehouse[new_y][new_x] == EMPTY:
+        return True
+    else:
+        return False
 
 
 def main():
@@ -257,24 +292,31 @@ def main():
     res = calc_checksum(warehouse)
     print('part 1:', res)
 
-    warehouse, movements = get_warehouse_and_movements('./test_inputs.txt')
+    warehouse, movements = get_warehouse_and_movements('./inputs.txt')
     warehouse = widen(warehouse)
     robot_position = get_robot_pos(warehouse)
     print(robot_position)
-    # print_warehouse(warehouse)
+
+    # print(print_warehouse(warehouse))
+    # boxes = find_all_boxes('^', (5, 6), warehouse)
+    # print(boxes)
+    print_warehouse(warehouse)
     for movement in movements:
         warehouse, robot_position = do_movement_wide(
             warehouse, robot_position, movement)
-        print('move', movement, ':')
-        print(print_warehouse(warehouse))
-        print('\n')
-        input('enter')
-
+        # print('move', movement, ':')
         # print(print_warehouse(warehouse))
-        # with open('out.txt', 'w') as file:
-        #     file.write(print_warehouse(warehouse))
+        # print('\n')
         # input('enter')
+    #
+    #     # print(print_warehouse(warehouse))
+    #     # with open('out.txt', 'w') as file:
+    #     #     file.write(print_warehouse(warehouse))
+    #     # input('enter')
+    #
 
+    print(print_warehouse(warehouse))
+    print('\n')
     res = calc_checksum_wide(warehouse)
     print('part 2:', res)
     return
