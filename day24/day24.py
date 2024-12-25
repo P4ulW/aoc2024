ec0f5de20d9b546bbd46eabc286496f3a0fa5f11
Paul Werner <paul.werner@studi.uni-goettingen.de>
2024-12-25 01:26:39 +0100

    solved part 2 manually with online help

--- day24/day24.py
+++ day24/day24.py
@@ -1,6 +1,9 @@
 from typing import Literal
+from re import findall
+from random import randint
 from rich import print
 from collections import defaultdict
+from itertools import combinations
 
 
 def AND(left, right):
@@ -43,43 +46,81 @@ def main():
 
     # print(wire_states)
     # print(to_calculate)
-    #
-    # for wire in to_calculate.keys():
-    #     state = calculate_wire(wire, wire_states, to_calculate)
-    #     wire_states[wire] = state
-    #
-    # num = ''
-    # for i in range(99, -1, -1):
-    #     key = f'z{str(i).zfill(2)}'
-    #     if key not in wire_states:
-    #         continue
-    #     state = str(wire_states[key])
-    #     num += state
-    #
-    # print(wire_states)
-    # print(int(num, 2))
 
-    res = correct_result(10, 0, init_wires, to_calculate)
-    print(res)
+    for wire in to_calculate.keys():
+        state = calculate_wire(wire, wire_states, to_calculate)
+        wire_states[wire] = state
+
+    num = ''
+    for i in range(99, -1, -1):
+        key = f'z{str(i).zfill(2)}'
+        if key not in wire_states:
+            continue
+        state = str(wire_states[key])
+        num += state
+
+    # print(wire_states)
+    print("res part 1:", int(num, 2))
+
+
+    to_calculate = swap_nodes('thm','z08',to_calculate)
+    to_calculate = swap_nodes('wss','wrm',to_calculate)
+    to_calculate = swap_nodes('z22','hwq',to_calculate)
+    to_calculate = swap_nodes('z29','gbs',to_calculate)
+
+    # tree = get_wire_diagramm(f'z29', to_calculate, 2)
+    # print(tree)
+    #
+    # for i in range(45):
+    #     tree = get_wire_diagramm(f'z{str(i).zfill(2)}', to_calculate, 2)
+    #     print(tree)
+
+    # for i in range(45):
+    #     res = run_random_tests(i, 100, init_wires, to_calculate)
+    #     print(i, res)
+
+    swaps = sorted(['thm', 'z08', 'wss', 'wrm', 'z22', 'hwq', 'z29', 'gbs'])
+    res2 = ",".join(list(swaps))
+    print('res part 2:', res2)
+
+                
+
+
     return
+
+def swap_nodes(left, right, to_calculate):
+    temp = to_calculate[left]
+    to_calculate[left] = to_calculate[right]
+    to_calculate[right] = temp
+    return to_calculate
+
+
+def run_random_tests(max_bit, num_test, init_wires, to_calculate):
+    for i in range(num_test):
+        x = randint(0, 2**max_bit-1)
+        y = randint(0, 2**max_bit-1)
+        success = correct_result(x, y, init_wires, to_calculate)
+        if not success:
+            return False
+    return True
 
 
 def set_wires_to_int(num: int, key: Literal['x', 'y'], wire_states):
-    num = bin(num).removeprefix('0b').zfill(44)
-    for idx in range(43, -1, -1):
-        wire = key+str(idx).zfill(2)
+    num = bin(num).removeprefix('0b').zfill(45)
+    for idx, gnum in enumerate(range(44, -1, -1)):
+        wire = key+str(gnum).zfill(2)
         wire_states[wire] = int(num[idx])
 
 
-def get_wire_diagramm(wire, wire_states, to_calculate):
+def get_wire_diagramm(wire, to_calculate, depth):
     out = ''
 
     left, op, right = to_calculate[wire]
-    if not (left.startswith('x') or left.startswith('y')):
-        left = get_wire_diagramm(left, wire_states, to_calculate)
+    if depth>0 and not (left.startswith('x') or left.startswith('y')):
+        left = get_wire_diagramm(left, to_calculate, depth-1)
 
-    if not (right.startswith('x') or right.startswith('y')):
-        right = get_wire_diagramm(right, wire_states, to_calculate)
+    if depth>0 and not (right.startswith('x') or right.startswith('y')):
+        right = get_wire_diagramm(right, to_calculate, depth-1)
 
     out += f'{wire} = ({left}) {op} ({right})'
 
@@ -98,7 +139,7 @@ def correct_result(x, y, init_wires, to_calculate):
         state = calculate_wire(wire, states, to_calculate)
         states[wire] = state
 
-    num = ''
+    num = '0b'
     for i in range(99, -1, -1):
         key = f'z{str(i).zfill(2)}'
         if key not in states:
@@ -106,23 +147,11 @@ def correct_result(x, y, init_wires, to_calculate):
         state = str(states[key])
         num += state
 
-    num = int(num)
-    print(num)
+    num = int(num, 2)
     if num == x+y:
         return True
 
     return False
-
-
-def input_to_zero(wire_states):
-    for key in wire_states.keys():
-        if 'x' in key:
-            wire_states[key] = 0
-
-        if 'y' in key:
-            wire_states[key] = 0
-    return wire_states
-
 
 def calculate_wire(wire, wire_states, to_calculate):
     state = 0
